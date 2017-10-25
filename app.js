4a575de3e221a9f45dfadc12d27819526fcb4d66
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-10-24 23:42:40 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// Getting started with select restaurant page
+// create react component
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ReactClass = React.createClass({
+  render: function () {
+    return React.createElement('h1', { className: 'header' }, 'React Component');
+  }
+});
+var reactComponentElement = React.createElement(ReactClass);
+var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
