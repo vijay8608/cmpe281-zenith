a1d40c1781104d464c9d8771afb82acf4ae70489
Sayali More <sayalimore8722@gmail.com>
2017-10-25 23:19:58 -0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <Header/>
+            <Content/>
+         </div>
+      );
+   }
+}
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Restaurants View</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>Starbucks</p>
+         </div>
+      );
+   }
+}
+
+export default App;
