f38729482193e7382fd167c8a397873a5c6e22a3
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-10-25 23:43:50 -0700

    logic to manage state

--- app.js
+++ app.js
@@ -9,4 +9,22 @@ var ReactClass = React.createClass({
   }
 });
 var reactComponentElement = React.createElement(ReactClass);
+//manage state
 var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
+{
+  getInitialState: function () {
+    return {
+      isHidden: false
+    };
+  },
+
+  render: function () {
+    if (this.state.isHidden) {
+      return null;
+    }
+
+    return React.createElement('h1', { className: 'header' }, 'React Component');
+  }
+}
+
+
