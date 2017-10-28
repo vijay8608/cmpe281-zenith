d42855fb951998d65bf7df63917026f01b073824
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-10-27 23:52:26 -0700

    Create restaurant.js

---
+++ UI Wireframes/Select Restaurant/restaurant.js
@@ -0,0 +1,26 @@
+mport React from 'react';
+import ReactDOM from 'react-dom';
+import {connect} from 'react-redux';
+import {addTodo} from "./actions";
+import {doneTodo} from "./actions";
+
+let x=0;
+
+class dvdStore extends React.Component{
+	
+	constructor(){
+		super();
+		this.state = {
+			value: 'this is the default state.'
+		}
+		
+	}
+
+
+				
+	render(){
+        return(
+        //display restaurant details
+        );
+    }
+  }
