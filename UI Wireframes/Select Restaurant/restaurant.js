e8a1d01839b5c752759ae1ba419ccad7c0e2d2ee
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-10-28 23:36:58 -0700

    add restaurant details

--- UI Wireframes/Select Restaurant/restaurant.js
+++ UI Wireframes/Select Restaurant/restaurant.js
@@ -21,6 +21,36 @@ class restaurant extends React.Component{
 	render(){
         return(
         //display restaurant details
+		<div>
+			<div className="menu">
+				<h3>Menu</h3>
+				<div className="appTitle">
+					<div className="item">
+						<span className="itemName">StarBucks</span><a>$20.00</a>
+						<button
+							onClick={() => {
+                                this.props.addTodo("Starbucks");
+                                addQuantity("StarBucks");
+                                newTotal(20);
+								}}>+</button>
+						
+					</div>
+					<p>
+					<div className="item">
+						<span className="itemName">Subway $20.00</span>
+						<button
+							onClick={() => {
+                                this.props.addTodo("Subway");
+								addQuantity("Subway");
+                                newTotal(20)
+                            }}>+</button>
+						
+					</div>
+					</p>
+				</div>	
+		
+			</div>
+</div>
         );
     }
   }
