f2d37d4e23739e21f8cd7ce6bb883afbfc6f43a3
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-11-03 23:45:39 -0700

    add restaurant

--- select-restaurant/src/App.js
+++ select-restaurant/src/App.js
@@ -5,15 +5,37 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      //display restaurant details
+		<div>
+			<div className="menu">
+				<h3>Menu</h3>
+				<div className="appTitle">
+					<div className="item">
+						<span className="itemName">StarBucks</span><a>$20.00</a>
+						<button
+							onClick={() => {
+                                this.props.addTodo("Starbucks");
+                                addQuantity("StarBucks");
+                                newTotal(20);
+								}}>+</button>
+						
+					</div>
+					<p>
+					<div className="item">
+						<span className="itemName">Subway $20.00</span>
+						<button
+							onClick={() => {
+                                this.props.addTodo("Subway");
+								addQuantity("Subway");
+                                newTotal(20)
+                            }}>+</button>
+						
+					</div>
+					</p>
+				</div>	
+		
+			</div>
+</div>
     );
   }
 }
