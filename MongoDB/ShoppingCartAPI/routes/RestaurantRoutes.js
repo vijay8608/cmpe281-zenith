80bef8be9b2abb07cbdbf85498a9a5b2af809708
snehalphatangare <snehalphatangare@gmail.com>
2017-11-10 22:14:12 -0800

    Create RestaurantRoutes.js

---
+++ MongoDB/ShoppingCartAPI/routes/RestaurantRoutes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function(app) {
+  var restaurantCtrl = require('../controllers/RestaurantController');
+
+  //app.route('/')
+    //.get(restaurantCtrl.justGet);
+
+  // Restaurant Routes
+  app.route('/restaurants')
+    .get(restaurantCtrl.list_all_restaurants);
+    //.post(restaurantCtrl.create_a_resta);
