cc79cd55d942829910b6d6ec153084cf4268d41a
Sayali More <sayalimore8722@gmail.com>
2017-11-11 15:10:54 -0800

    Update Order.js

--- Order.js
+++ Order.js
@@ -10,7 +10,13 @@ class Order extends Component {
     render() {
         return (
             <div className="row justify-content-md-center">
-                
+            <div className="col-md-3">
+                    {this.props.message && ( //Just a change here
+                        <div className="alert alert-warning" role="alert">
+                            {this.props.message}
+                        </div>
+                    )}
+                </div>    
             </div>
         );
     }
