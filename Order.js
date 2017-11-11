2bf2a5826cf773724262c89928a4b36cffb0b975
Sayali More <sayalimore8722@gmail.com>
2017-11-11 15:10:34 -0800

    Create Order.js

---
+++ Order.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Order extends Component {
+
+    static propTypes = {
+        message: PropTypes.string.isRequired
+    };
+
+    render() {
+        return (
+            <div className="row justify-content-md-center">
+                
+            </div>
+        );
+    }
+}
+
+export default Order;
