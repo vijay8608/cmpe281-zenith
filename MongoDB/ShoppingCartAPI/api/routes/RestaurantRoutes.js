ca9fbcd894a148c7e3cd51819f96897d6c1f2dc3
snehalphatangare <snehalphatangare@gmail.com>
2017-11-17 21:56:15 -0800

    Added routes for MenuItems

--- MongoDB/ShoppingCartAPI/api/routes/RestaurantRoutes.js
+++ MongoDB/ShoppingCartAPI/api/routes/RestaurantRoutes.js
@@ -2,13 +2,20 @@
 
 module.exports = function(app) {
   var restaurantCtrl = require('../controllers/RestaurantController');
+  var menuItemCtrl = require('../controllers/MenuItemController');
 
-  app.route('/')
-    .get(restaurantCtrl.justGet);
+  //app.route('/')
+  //  .get(restaurantCtrl.justGet);
 
   // Restaurant Routes
   app.route('/restaurants')
     .get(restaurantCtrl.list_all_restaurants);
-    //.post(restaurantCtrl.create_a_resta);
+
+  app.route('/restaurants/:id')
+    .get(restaurantCtrl.get_a_restaurant);
+
+  // Menu Routes
+   app.route('/menuItems')
+  	.get(menuItemCtrl.list_all_menuItems);
 
 };
