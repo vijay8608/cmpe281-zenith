4ffa602c33603a7d0297d46741bb79e5bf26d678
snehalphatangare <snehalphatangare@gmail.com>
2017-11-17 21:59:11 -0800

    Update MenuItemController.js

--- MongoDB/ShoppingCartAPI/api/controllers/MenuItemController.js
+++ MongoDB/ShoppingCartAPI/api/controllers/MenuItemController.js
@@ -1,27 +1,33 @@
 'use strict';
 var mongoose = require('mongoose');
 
-var menuItems = mongoose.model('menuItems');
+//var menuItems = mongoose.model('menuItems');
+var menu = mongoose.model('menu1');
+
 
 exports.list_all_menuItems = function(req, res) {
-
-/*tests.find({}, function(err, result) {
-	if (err)
-    {
-      console.log("-------In error-----");
-      res.send(err);
-    }
-  	console.log("*********in Test",result);
-});
-*/
-  menuItems.find({}, function(err, result) {
+  menu.find(function(err, result) {
     if (err)
       {
         console.log("-------In error-----");
         res.send(err);
       }
-      console.log("*********in get all menuItems",result);
+      console.log("*********in get all menuItems",result.length);
+      res.json(result);
   });
+
+  exports.get_items_restaurant = function(req, res) {
+	  console.log("******Restaurant RId=",req.params.RId);
+	  menu.find({restaurant_id:req.params.RId}, function(err, result) {
+	    if (err)
+	      {
+	        console.log("-------Error in get_a_restaurant-----");
+	        res.send(err);
+	      }
+	      console.log("*********in get_items_restaurant",result);
+	      res.json(result);
+	  });
+	};
 
 };
 
