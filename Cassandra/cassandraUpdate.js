6dda71959e43499bb1e8be5b7c98addbcfa32913
Sachin Dadaso Waghmode <sachinwaghmode57@gmail.com>
2017-11-18 23:35:07 -0800

    syntax error

--- Cassandra/cassandraUpdate.js
+++ Cassandra/cassandraUpdate.js
@@ -7,7 +7,7 @@ var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'door
 
 // Read users and update to doordash
    function (callback) {
-       client.execute("update user_doordash set order_quantity =2 WHERE restaurant_id='Starbucks001', function (err, result) {
+       client.execute("update user_doordash set order_quantity =2 WHERE restaurant_id='Starbucks001'", function (err, result) {
            if (!err){
                
                    console.log("Successful update into doordash");
